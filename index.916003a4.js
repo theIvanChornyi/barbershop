c91b4f49f737f5e2378ca642993d59083b9aab37
theIvanChornyi <107806721+theIvanChornyi@users.noreply.github.com>
2022-08-03 19:32:53 +0000

    Deploying to gh-pages from @ theIvanChornyi/barbershop@e49eeeebe889f83463406477b5d9084837d21c86 🚀

---
+++ index.916003a4.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[menu-open]"),closeModalBtn:document.querySelector("[menu-close]"),modal:document.querySelector("[data-menu]")};function n(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",n),e.closeModalBtn.addEventListener("click",n)})();
+//# sourceMappingURL=index.916003a4.js.map
---
+++ index.a20b7918.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[menu-open]"),closeModalBtn:document.querySelector("[menu-close]"),modal:document.querySelector("[data-menu]")};function n(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",n),e.closeModalBtn.addEventListener("click",n)}();
+//# sourceMappingURL=index.a20b7918.js.map
